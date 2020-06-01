c81e8ac42ff68ea061b9acf5389b519bb025c12c
SimoneBallesio <esse.ballesio@gmail.com>
2020-06-01 21:38:08 +0200

    Added premake support

--- premake5.lua
+++ premake5.lua
@@ -16,7 +16,12 @@ project "imgui"
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+    "imgui_demo.cpp"
+  }
+  
+  defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"
 	}
 
 	filter "system:windows"
